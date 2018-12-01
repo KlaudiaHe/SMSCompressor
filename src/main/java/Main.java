fcf172ff5505b3968d0c73bb7fb04fd7a7c3cde4
Klaudia <khturowska@gmail.com>
2018-12-01 11:52:24 +0100

    Changes to files

---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class smsCompressor implements SMSCompression {
+
+    public static void main(String[] args) {
+        System.out.println("Type in your message: ");
+        Scanner input = new Scanner(System.in);
+        String message = input.next();
+/*
+        String[] texts = text.split(" ");
+        text = text.substring(0,1).toUpperCase() + text.substring(1);
+
+        for (int i = 0; i < texts.length; i++) {
+*/
+        smsCompressor smsCompressor = new smsCompressor();
+               smsCompressor(message);
+
+        }
+
+    @Override
+    public String smsCompressor(String input) {
+            InputStreamReader message = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(message);
+            br.lines()
+                    .map(line -> Stream.of(line.split("\\s+")).map(w -> w.substring(0, 1).toUpperCase() + w.substring(1)).collect(Collectors.joining("")))
+                    .forEach(System.out::println);
+        return null;
+    }
+
+    @Override
+    public String smsDecompressor(String input) {
+        return null;
+    }
+}
+/*
+compressor- interface; klasa naiveCompressor z nastoletnią metodą; advancedCompressor;
+//costCalculator; klasa;
+//Paginator- kawalki o stalej dlugosci; klasa;
+//DDD- domain driven design;
+*/
