c36edc827893213e767572537c70906b65147bdc
Klaudia <khturowska@gmail.com>
2019-01-13 12:40:12 +0100

    struggle with jar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,9 @@
-import Compressor.Compressor;
-import Compressor.NaiveCompressor;
-import Compressor.OptimalCompressor;
+import picocli.CommandLine;
 
-import java.math.BigDecimal;
-import java.util.Collection;
-import java.util.Scanner;
-
-public class Main{
+public class Main {
     public static void main(String[] args) {
-
-        int SMS_LENGTH= 160;
-        BigDecimal SMS_PRICE = BigDecimal.valueOf(0.3);
-
-        System.out.println("Type in your message: ");
-        Scanner input = new Scanner(System.in);
-        String message = input.nextLine();
-        System.out.println("Choose a type of compression (n = naive; o = optimal): ");
-        String compressorType = input.nextLine();
-        Paginator paginator = new Paginator(SMS_LENGTH);
-        CostCalculator calculator = new CostCalculator(SMS_PRICE);
-        Compressor compressor;
-
-        switch (compressorType){
-            case "n": compressor = new NaiveCompressor();
-                break;
-            case "o": compressor = new OptimalCompressor();
-                break;
-            default: throw new IllegalArgumentException();
-        }
-
-        String compressed = compressor.smsCompressor(message);
-        Collection<String> paginated = paginator.paginate(compressed);
-        System.out.println(paginated);
-        System.out.println("Cost of your message(s): " + calculator.calculateCost(paginated));
-        String decompressedAgain = compressor.smsDecompressor(compressed);
-        System.out.println("Here's your message decompressed again: "+decompressedAgain);
-
+        PicocliApp picocliApp = new PicocliApp();
+        new CommandLine(picocliApp).parse(args);
+        picocliApp.run();
     }
-
-
 }
--- src/main/java/PicocliMain.java
+++
@@ -1,9 +0,0 @@
-import picocli.CommandLine;
-
-public class PicocliMain {
-    public static void main(String[] args) {
-        PicocliApp cla = new PicocliApp();
-        new CommandLine(cla).parse(args);
-        cla.run();
-    }
-}
